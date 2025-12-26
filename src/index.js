584aaeb3b8dd036f5055c993aeb0829c8525a976
Lisonnnnn <sabbirhasan22k@gmail.com>
2025-12-26 21:56:56 +0600

    mulitple elements bind in single element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 var React = require("react");
 var ReactDom = require("react-dom");
-ReactDom.render(<h1>Hello World</h1>, document.getElementById("root"));
+ReactDom.render(<div>
+    <h1>Hello World</h1>
+    <p>This is a paragraph</p>
+
+</div>, document.getElementById("root"));
